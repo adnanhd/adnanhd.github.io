8b404f3831968b59c853d6b13954423089800e34
adnanhd <adnanharundogan@gmail.com>
2026-03-17 15:18:36 +0300

    feat: add Google Search Console verification file

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ DEST="${1:?Usage: $0 user@host:/path/to/webroot}"
 echo "Deploying to $DEST..."
 scp -r index.html style.css data.js sitemap.xml robots.txt 404.html \
     resume.pdf cv.pdf profile.jpeg profile-sm.jpeg \
+    googlefdcfc688492f74e0.html \
     images/ blogs/ "$DEST"
 
 echo "Done."
